ba99c99afb0dc7fa4a05aafc9dcbaa2d0a317981
Tam <tamborinehands@gmail.com>
2016-10-20 12:25:34 +0100

    backbone of tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require 'game'
+
+describe Game do
+  it "players can take turns to mark grid field"
+
+  it "a turn ends when a player marks a grid field"
+
+  it "a player wins if marked all fields in a row, column or diagonal"
+
+  it "game ends when a player wins"
+
+  it "game ends when all fields are taken"
+
+  it "player can claim a field on grid if not taken"
+  it "is easy to hook up to a UI"
+end
